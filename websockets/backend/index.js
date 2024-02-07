cc1f580cf787a289fd6aadd47f6631e0cca7262b
Joen Tan <joentze01@gmail.com>
2024-02-07 20:51:57 +0800

    feat: added cors allowing all origins in the backend

--- websockets/backend/index.js
+++ websockets/backend/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const http = require("http");
+const cors = require('cors');
 const { Server } = require("socket.io");
 
 const app = express();
+
+app.use(cors())
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+  cors: {
+    origin: "*", // Allow all origins
+  }
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected");
---
+++ websockets/frontend/script.js
@@ -0,0 +1,5 @@
+const HOST = "http://localhost:3000"
+
+const socket = io(HOST);
+
+
